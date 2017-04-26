96bec3c0520b9aeeec12cd3122f06db089984e49
Riyaz Ahamed <dev.ahamed@gmail.com>
2017-04-26 22:14:59 +0530

    Fix the item decoration issue when the adapter has 'reverse layout' option in the LayoutManager

--- multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/ItemDecorationManager.java
+++ multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/ItemDecorationManager.java
@@ -2,6 +2,7 @@ package com.ahamed.multiviewadapter;
 
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -20,28 +21,42 @@ class ItemDecorationManager extends RecyclerView.ItemDecoration {
 
   @Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
       RecyclerView.State state) {
+    boolean isReverseLayout = getIsReverseLayout(parent);
     ItemBinder binder = adapter.getBinderForPosition(parent.getChildAdapterPosition(view));
     if (binder.isItemDecorationEnabled()) {
       int itemPosition = adapter.getItemPositionInManager(parent.getChildAdapterPosition(view));
-      boolean isLastItem = adapter.isLastItemInManager(parent.getChildAdapterPosition(view));
-      int positionType = itemPosition == 0 ? ItemDecorator.POSITION_START
-          : isLastItem ? ItemDecorator.POSITION_END : ItemDecorator.POSITION_MIDDLE;
+      int positionType = getPositionType(parent, view, itemPosition, isReverseLayout);
       binder.getItemOffsets(outRect, itemPosition, positionType);
     }
   }
 
   @Override public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+    boolean isReverseLayout = getIsReverseLayout(parent);
     int childCount = parent.getChildCount();
-    for (int i = 0; i < childCount - 1; i++) {
+    for (int i = 0; i < childCount; i++) {
       View child = parent.getChildAt(i);
       ItemBinder binder = adapter.getBinderForPosition(parent.getChildAdapterPosition(child));
       if (binder.isItemDecorationEnabled()) {
         int itemPosition = adapter.getItemPositionInManager(parent.getChildAdapterPosition(child));
-        boolean isLastItem = adapter.isLastItemInManager(parent.getChildAdapterPosition(child));
-        int positionType = itemPosition == 0 ? ItemDecorator.POSITION_START
-            : isLastItem ? ItemDecorator.POSITION_END : ItemDecorator.POSITION_MIDDLE;
+        int positionType = getPositionType(parent, child, itemPosition, isReverseLayout);
         binder.onDraw(canvas, parent, child, itemPosition, positionType);
       }
     }
+  }
+
+  private boolean getIsReverseLayout(RecyclerView parent) {
+    return parent.getLayoutManager() instanceof LinearLayoutManager && ((LinearLayoutManager) parent
+        .getLayoutManager()).getReverseLayout();
+  }
+
+  private @ItemDecorator.PositionType int getPositionType(RecyclerView parent, View child,
+      int itemPosition, boolean isReverseLayout) {
+    boolean isFirstItem =
+        isReverseLayout ? adapter.isLastItemInManager(parent.getChildAdapterPosition(child))
+            : itemPosition == 0;
+    boolean isLastItem = isReverseLayout ? itemPosition == 0
+        : adapter.isLastItemInManager(parent.getChildAdapterPosition(child));
+    return isFirstItem ? ItemDecorator.POSITION_START
+        : isLastItem ? ItemDecorator.POSITION_END : ItemDecorator.POSITION_MIDDLE;
   }
 }
--- multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/ItemDecorator.java
+++ multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/ItemDecorator.java
@@ -14,25 +14,6 @@ public interface ItemDecorator {
   int POSITION_MIDDLE = 2;
   int POSITION_END = 3;
 
-  /**
-   * Retrieve any offsets for the given item. Each field of <code>outRect</code> specifies
-   * the number of pixels that the item view should be inset by, similar to padding or margin.
-   * The default implementation sets the bounds of outRect to 0 and returns.
-   *
-   * <p>
-   * If this ItemDecoration does not affect the positioning of item views, it should set
-   * all four fields of <code>outRect</code> (left, top, right, bottom) to zero
-   * before returning.
-   *
-   * <p>
-   * If you need to access Adapter for additional data, you can call
-   * {@link RecyclerView#getChildAdapterPosition(View)} to get the adapter position of the
-   * View.
-   *
-   * @param outRect Rect to receive the output.
-   * @param position Position of the item inside the {@link DataListManager}
-   * @param positionType Represents the {@link PositionType} of the item.
-   */
   void getItemOffsets(Rect outRect, int position, @PositionType int positionType);
 
   void onDraw(Canvas canvas, RecyclerView parent, View child, int position,
