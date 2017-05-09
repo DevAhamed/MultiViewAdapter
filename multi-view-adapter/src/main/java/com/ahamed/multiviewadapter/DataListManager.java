2eef3c55c0ff5c400608f82821d92f6a31cb3b90
Riyaz Ahamed <dev.ahamed@gmail.com>
2017-05-09 12:29:14 +0530

    Add PayloadProvider for resolving the payloads inside the DataListManager

--- multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/DataListManager.java
+++ multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/DataListManager.java
@@ -6,10 +6,25 @@ import android.support.v7.widget.RecyclerView;
 import java.util.Collection;
 import java.util.List;
 
-public class DataListManager<M> extends BaseDataManager<M> {
+public final class DataListManager<M> extends BaseDataManager<M> {
 
-  public DataListManager(RecyclerAdapter baseAdapter) {
-    super(baseAdapter);
+  private final PayloadProvider<M> payloadProvider;
+
+  public DataListManager(RecyclerAdapter adapter) {
+    this(adapter, new PayloadProvider<M>() {
+      @Override public boolean areContentsTheSame(M oldItem, M newItem) {
+        return oldItem.equals(newItem);
+      }
+
+      @Override public Object getChangePayload(M oldItem, M newItem) {
+        return null;
+      }
+    });
+  }
+
+  public DataListManager(RecyclerAdapter adapter, PayloadProvider<M> payloadProvider) {
+    super(adapter);
+    this.payloadProvider = payloadProvider;
   }
 
   /**
@@ -108,7 +123,7 @@ public class DataListManager<M> extends BaseDataManager<M> {
   public final void set(int index, M item) {
     M oldItem = getDataList().get(index);
     getDataList().set(index, item);
-    onChanged(index, 1, getChangePayload(oldItem, item));
+    onChanged(index, 1, payloadProvider.getChangePayload(oldItem, item));
   }
 
   /**
@@ -121,11 +136,11 @@ public class DataListManager<M> extends BaseDataManager<M> {
     DiffUtil.DiffResult result =
         DiffUtil.calculateDiff(new DiffUtilCallback<M>(this.getDataList(), dataList) {
           @Override public boolean areContentsTheSame(M oldItem, M newItem) {
-            return DataListManager.this.areContentsTheSame(oldItem, newItem);
+            return payloadProvider.areContentsTheSame(oldItem, newItem);
           }
 
           @Override public Object getChangePayload(M oldItem, M newItem) {
-            return DataListManager.this.getChangePayload(oldItem, newItem);
+            return payloadProvider.getChangePayload(oldItem, newItem);
           }
         });
     setDataList(dataList);
@@ -180,5 +195,40 @@ public class DataListManager<M> extends BaseDataManager<M> {
     int oldSize = getDataList().size();
     getDataList().clear();
     onRemoved(0, oldSize);
+  }
+
+  public interface PayloadProvider<M> {
+
+    /**
+     * Called by the {@link BaseDataManager} when it wants to check whether two items have the same
+     * data.
+     * BaseDataManager uses this information to detect if the contents of an item has changed.
+     * <p>
+     * BaseDataManager uses this method to check equality instead of {@link Object#equals(Object)}
+     * so that you can change its behavior depending on your UI.
+     *
+     * @param oldItem The item in the old list
+     * @param newItem The item in the new list which replaces the oldItem
+     * @return True if the contents of the items are the same or false if they are different, ie.,
+     * you
+     * should return whether the items' visual representations are the same.
+     */
+    boolean areContentsTheSame(M oldItem, M newItem);
+
+    /**
+     * Called by the {@link BaseDataManager} when it wants to get the payload of changed elements.
+     * <p>
+     * For example, if you are using DiffUtil with {@link RecyclerView}, you can return the
+     * particular field that changed in the item and your
+     * {@link android.support.v7.widget.RecyclerView.ItemAnimator ItemAnimator} can use that
+     * information to run the correct animation.
+     * <p>
+     * Default implementation returns {@code null}.
+     *
+     * @param oldItem The item in the old list
+     * @param newItem The item in the new list
+     * @return A payload object that represents the change between the two items.
+     */
+    @SuppressWarnings("UnusedParameters") Object getChangePayload(M oldItem, M newItem);
   }
 }
