1bd7c396de4c32d5a077a3be8e6b0e070d55222a
Riyaz Ahamed <dev.ahamed@gmail.com>
2017-05-19 00:31:45 +0530

    Update sample code to demonstrate DataBinding support

--- sample/src/main/java/com/ahamed/sample/simple/QuoteBinder.java
+++ sample/src/main/java/com/ahamed/sample/simple/QuoteBinder.java
@@ -1,37 +1,33 @@
 package com.ahamed.sample.simple;
 
+import android.databinding.DataBindingUtil;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import com.ahamed.multiviewadapter.BaseViewHolder;
-import com.ahamed.multiviewadapter.ItemBinder;
+import com.ahamed.multiviewadapter.BindingViewHolder;
+import com.ahamed.multiviewadapter.DataBinder;
 import com.ahamed.sample.R;
+import com.ahamed.sample.databinding.ItemQuoteBinding;
 
-public class QuoteBinder extends ItemBinder<Quote, QuoteBinder.ItemViewHolder> {
+public class QuoteBinder extends DataBinder<Quote, ItemQuoteBinding, QuoteBinder.ItemViewHolder> {
 
   @Override public ItemViewHolder create(LayoutInflater layoutInflater, ViewGroup parent) {
-    return new ItemViewHolder(layoutInflater.inflate(R.layout.item_quote, parent, false));
+    ItemQuoteBinding binding =
+        DataBindingUtil.inflate(layoutInflater, R.layout.item_quote, parent, false);
+    return new ItemViewHolder(binding);
   }
 
   @Override public boolean canBindData(Object item) {
     return item instanceof Quote;
   }
 
-  @Override public void bind(ItemViewHolder holder, Quote item) {
-    holder.tvQuotes.setText(item.getQuote());
-    holder.tvAuthor.setText(item.getAuthor());
-  }
+  static class ItemViewHolder extends BindingViewHolder<Quote, ItemQuoteBinding> {
 
-  static class ItemViewHolder extends BaseViewHolder<Quote> {
+    ItemViewHolder(ItemQuoteBinding binding) {
+      super(binding);
+    }
 
-    private TextView tvQuotes;
-    private TextView tvAuthor;
-
-    ItemViewHolder(View itemView) {
-      super(itemView);
-      tvQuotes = (TextView) itemView.findViewById(R.id.tv_quotes);
-      tvAuthor = (TextView) itemView.findViewById(R.id.tv_author);
+    @Override public void bind(ItemQuoteBinding binding, Quote item) {
+      binding.setQuote(item);
     }
   }
 }
