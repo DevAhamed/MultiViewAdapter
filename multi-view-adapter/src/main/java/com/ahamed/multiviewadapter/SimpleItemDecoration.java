dfc82a5db624a27ea8ddc31a27d46ae9a23856f2
Riyaz Ahamed <dev.ahamed@gmail.com>
2017-05-24 00:16:18 +0530

    Deprecate SimpleItemDecoration properly

--- multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/SimpleItemDecoration.java
+++ multi-view-adapter/src/main/java/com/ahamed/multiviewadapter/SimpleItemDecoration.java
@@ -3,7 +3,8 @@ package com.ahamed.multiviewadapter;
 import android.content.Context;
 import com.ahamed.multiviewadapter.util.SimpleDividerDecoration;
 
-@Deprecated public class SimpleItemDecoration extends SimpleDividerDecoration {
+@Deprecated public class SimpleItemDecoration extends SimpleDividerDecoration
+    implements ItemDecorator {
 
   public SimpleItemDecoration(Context context, int orientation) {
     super(context, orientation);
