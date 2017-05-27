80016e20ffd9f1e45122c3a382e253a3ea0328fc
Riyaz Ahamed <dev.ahamed@gmail.com>
2017-05-27 13:34:14 +0530

    Update DataBinding sample

--- sample/src/main/java/com/ahamed/sample/data/binding/QuoteDataBinder.java
+++ sample/src/main/java/com/ahamed/sample/data/binding/QuoteDataBinder.java
@@ -19,33 +19,23 @@ package com.ahamed.sample.data.binding;
 import android.databinding.DataBindingUtil;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import com.ahamed.multiviewadapter.BindingViewHolder;
 import com.ahamed.multiviewadapter.ItemDataBinder;
 import com.ahamed.sample.R;
 import com.ahamed.sample.common.model.Quote;
 import com.ahamed.sample.databinding.ItemBindingQuoteBinding;
 
-public class QuoteDataBinder
-    extends ItemDataBinder<Quote, ItemBindingQuoteBinding, QuoteDataBinder.ViewHolder> {
+public class QuoteDataBinder extends ItemDataBinder<Quote, ItemBindingQuoteBinding> {
 
-  @Override public ViewHolder create(LayoutInflater inflater, ViewGroup parent) {
-    ItemBindingQuoteBinding quoteBinding =
-        DataBindingUtil.inflate(inflater, R.layout.item_binding_quote, parent, false);
-    return new ViewHolder(quoteBinding);
+  @Override protected void bindModel(Quote item, ItemBindingQuoteBinding binding) {
+    binding.setQuoteModel(item);
+  }
+
+  @Override
+  protected ItemBindingQuoteBinding createBinding(LayoutInflater inflater, ViewGroup parent) {
+    return DataBindingUtil.inflate(inflater, R.layout.item_binding_quote, parent, false);
   }
 
   @Override public boolean canBindData(Object item) {
     return item instanceof Quote;
-  }
-
-  static class ViewHolder extends BindingViewHolder<Quote, ItemBindingQuoteBinding> {
-
-    public ViewHolder(ItemBindingQuoteBinding binding) {
-      super(binding);
-    }
-
-    @Override public void bind(ItemBindingQuoteBinding binding, Quote item) {
-      binding.setQuoteModel(item);
-    }
   }
 }
