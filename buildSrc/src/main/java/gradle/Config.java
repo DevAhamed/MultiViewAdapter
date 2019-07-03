4c940ecb1ab08308a7456c659f7ccf85b6a6438f
Riyaz Ahamed <dev.ahamed@gmail.com>
2019-07-04 00:17:30 +0530

    Prepare and release v2.0.0-beta01 (#91)
    1. Update version name in config
    2. Add changelog to readme and update version name

--- buildSrc/src/main/java/gradle/Config.java
+++ buildSrc/src/main/java/gradle/Config.java
@@ -24,7 +24,7 @@ public interface Config {
 
   String VERSION = "2.0.0";
   String APP_VERSION = VERSION + ".1";
-  String PRODUCTION_VERSION = VERSION + "-alpha02";
+  String PRODUCTION_VERSION = VERSION + "-beta01";
   String SNAPSHOT_VERSION = VERSION + "-SNAPSHOT";
   int VERSION_CODE = 210200002;
 
